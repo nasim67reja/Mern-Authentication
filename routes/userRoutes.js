fc823fecdbda593c7c82c2a06237abce6a63df91
Nasim Reja <nasimreja67@gmail.com>
2023-02-11 09:31:57 +0600

    send jwt via cookie

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,11 +1,83 @@
 const express = require("express");
-const authController = require("../controllers/authController");
+const User = require("../models/userModels");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const router = express.Router();
 
-// router.get("/", authController.signup);
+router.post("/signup", async (req, res) => {
+  try {
+    req.body.password = await bcrypt.hash(req.body.password, 12);
 
-router.post("/signup", authController.signup);
-router.get("/login", authController.login);
+    const newUser = await await User.create(req.body);
+
+    const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+      expiresIn: "1d",
+    });
+
+    newUser.password = undefined;
+
+    res.cookie("token", token, {
+      expires: new Date(Date.now() + 24 * 60 * 60 * 1000),
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+    });
+
+    res.status(200).json({
+      status: "success",
+      data: {
+        token,
+        user: newUser,
+      },
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: "error",
+      message: error.message,
+    });
+  }
+});
+
+router.get("/login", async (req, res) => {
+  try {
+    const userData = await User.findOne({
+      email: req.body.email,
+    });
+
+    if (!userData) throw new Error("Email not found");
+
+    const passwordMatch = await bcrypt.compare(
+      req.body.password,
+      userData.password
+    );
+
+    if (!passwordMatch) throw new Error("Password is incorrect");
+
+    const token = jwt.sign({ id: userData._id }, process.env.JWT_SECRET, {
+      expiresIn: "1d",
+    });
+
+    userData.password = undefined;
+
+    res.cookie("token", token, {
+      expires: new Date(Date.now() + 24 * 60 * 60 * 1000),
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+    });
+
+    res.status(200).json({
+      status: "success",
+      data: {
+        token,
+        data: userData,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: "error",
+      message: err.message,
+    });
+  }
+});
 
 module.exports = router;
